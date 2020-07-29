560ab6188d9ef1c9de86c35ad5103f0ef3a037b5
aytami <68582141+aytami@users.noreply.github.com>
2020-07-29 14:28:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyCjwbKUvDopzXFnl8KhYeYHj0YGhrSHUjI",
-      authDomain: "mydatabase-c1f53.firebaseapp.com",
-      databaseURL: "https://mydatabase-c1f53.firebaseio.com",
-      projectId: "mydatabase-c1f53",
-      storageBucket: "mydatabase-c1f53.appspot.com",
-      messagingSenderId: "925452085527"
-     };
-    firebase.initializeApp(config);
-  
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
 
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
-
-    alert("Active User + email_id);
 
     var user = firebase.auth().currentUser;
 
